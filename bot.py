f682201792cd758c3cc11639102f755dbff10201
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-13 16:09:15 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,21 @@
+import config
+import telebot
+
+
+bot = telebot.TeleBot(config.TOKEN)
+
+
+@bot.message_handler(commands=['start'])
+def wellcome(message):
+    bot.send_message(message.chat.id, 'Здарова прогеры, я крутой бот ! \nМеня создал гений')
+
+
+@bot.message_handler(content_types=['Здарова'])
+def hello(message):
+    bot.send_message(message.chat.id, f'Здарова {message.from_user.first_name}')
+
+
+@bot.message_handler(content_types=['text'])
+def test(message):
+    if 'тест222' in message.text.lower():
+        bot.send_message(message.chat.id, 'Тест')
