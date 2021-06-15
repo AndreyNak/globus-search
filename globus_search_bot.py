fc9d8475cc7721b45db483bf5dd691a3ba956eb7
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-15 19:18:33 +0300

    add handllers add & delete

--- globus_search_bot.py
+++ globus_search_bot.py
@@ -1,5 +1,9 @@
 import config
 import telebot
+from main import *
+from controllers.controller_type import ControllerTypeGoods
+conn = Con.connect()
+Con.m_cursor(conn)
 
 bot = telebot.TeleBot(config.TOKEN)
 
@@ -9,9 +13,46 @@ def wellcome(message):
     bot.send_message(message.chat.id, 'Здарова прогеры, я крутой бот ! \nМеня создал гений')
 
 
+@bot.message_handler(commands=['add','delete'])
+def add_type(message):
+    message_user = message.text
+    if (message_user == '/add'):
+        msg = bot.send_message(message.chat.id, 
+        'Напишите название типа товара, который хотите добавить')
+        bot.register_next_step_handler(msg, add)
+    elif (message_user == '/delete'):
+        msg = bot.send_message(message.chat.id, 
+        'Напишите название типа товара, который хотите удалить')
+        bot.register_next_step_handler(msg, delete)
+
+def add(message):
+    name_type = message.text
+    if (len(name_type)) < 1:
+        msg = bot.send_message(message, 'Ну напиши ты что нибудь !')
+        bot.register_next_step_handler(msg, add)
+        return
+    else:
+        obg = ControllerTypeGoods(name_type)
+        obg.add_item()
+        bot.send_message(message.chat.id, f'Тип с названием {name_type} \n - был добавлен в базу !')
+
+def delete(message):
+    name_type = message.text
+    if (len(name_type)) < 1:
+        msg = bot.send_message(message, 'Ну напиши ты что нибудь !')
+        bot.register_next_step_handler(msg, delete)
+        return
+    else:
+        obg = ControllerTypeGoods(name_type)
+        log = obg.delete_item()
+        bot.send_message(message.chat.id, log)   
+
 @bot.message_handler(content_types=['Здарова'])
 def hello(message):
     bot.send_message(message.chat.id, f'Здарова {message.from_user.first_name}')
+
+
+
 
 
 @bot.message_handler(content_types=['text'])
@@ -19,5 +60,8 @@ def test(message):
     if 'тест222' in message.text.lower():
         bot.send_message(message.chat.id, 'Тест')
 
+# bot.enable_save_next_step_handlers(delay=2)
+# bot.load_next_step_handlers()
 bot.polling()
+conn.close()
 
