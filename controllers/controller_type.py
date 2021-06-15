0b05f3cb8c3bcde6f823082ad1c6635dbc74d215
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-15 17:24:50 +0300

    make controllers: add, delete, update

--- controllers/controller_type.py
+++ controllers/controller_type.py
@@ -1,5 +1,5 @@
 from db.models.base_model import Types
-
+import peewee
 
 class ControllerTypeGoods:
     def __init__(self, name_type):
@@ -8,3 +8,20 @@ class ControllerTypeGoods:
     def add_item(self):
         article = Types(type=self.name_type)
         article.save()
+
+    
+    def delete_item(self):
+        try:
+            artist = Types.get(Types.type == self.name_type )
+        except Exception:
+            print('Не удален, потому что не найден !')
+        else:
+             artist.delete_instance()
+             
+
+    def update_item(self):
+        artistId = Types.get(Types.type == self.name_type)
+        new_word = input('На какое слово хочешь поменять ?')
+        artist = Types(type=new_word)
+        artist.id = artistId.id
+        artist.save()
