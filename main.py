06b6e978082fc3f6bd971b3da5014a0e3cc27829
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-15 19:23:37 +0300

    delete trash

--- main.py
+++ main.py
@@ -1,35 +1,13 @@
-from copy import Error
-from logging import error
 from controllers.controller_type import ControllerTypeGoods
 from db.connect import *
 
-conn = Con.connect()
-Con.m_cursor(conn)
-
-#--------------------Добавление--------------------
-# question = input('Хотите добавить тип товара ? ')
-# if question == 'да':
-#     obg = ControllerTypeGoods(input('Название типа: '))
-#     obg.add_item()
-# else:
-#     print('пока!')
-#     exit()
-#--------------------Добавление--------------------
-
-#--------------------Изменение--------------------
-# obg = ControllerTypeGoods(input('Название типа которое хочешь поменять: '))
-# obg.update_item()
-#--------------------Изменение--------------------
 
 
-#--------------------Удаление--------------------
-# question = input('Хотите удалить тип товара ? ')
-# if question == 'да':
-#     obg = ControllerTypeGoods(input('Название типа: '))
-#     obg.delete_item()   
-# else:
-#     print('пока!')
-#     exit() 
-#--------------------Удаление--------------------
-conn.close()
+
+def update():
+    #--------------------Изменение--------------------
+    obg = ControllerTypeGoods(input('Название типа которое хочешь поменять: '))
+    obg.update_item()
+    #--------------------Изменение--------------------
+
 
