a4be11d45ce116781660a54d857d4e0e8faef278
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-16 19:41:49 +0300

    upgrade delete methot

--- globus_search_bot.py
+++ globus_search_bot.py
@@ -9,11 +9,6 @@ Con.m_cursor(conn)
 bot = telebot.TeleBot(config.TOKEN)
 
 
-# ARRAY = {
-#     'Сыр': 'photos/cheese_zoom.jpg'
-# }
-
-
 # @bot.message_handler(commands=['start'])
 # def wellcome(message):
 #     rmk = types.ReplyKeyboardMarkup(resize_keyboard = True)
@@ -35,6 +30,9 @@ def wellcome(message):
     "Карта" - Показать карту глобуса\n
     "Здарова" - бот здоровается с тобой\n
     """)
+
+
+
 
 @bot.message_handler(content_types=['text'])
 def menu(message):
@@ -78,16 +76,27 @@ def check_item(message):
 def hello(message):
     bot.send_message(message.chat.id, f'Здарова {message.from_user.first_name}')
 
-def add(message):
-    name_type = message.text
-    if (len(name_type)) < 1:
-        msg = bot.send_message(message.chat.id, 'Ну напиши ты что нибудь !')
-        bot.register_next_step_handler(msg, add)
-        return
-    else:
-        obg = ControllerTypeGoods(name_type)
-        obg.add_item()
-        bot.send_message(message.chat.id, f'Тип с названием {name_type} \n - был добавлен в базу !')
+@bot.message_handler(content_types=['document'])
+def photo_set(message):
+    name_type = message.caption
+    raw = message.document.file_id
+    path = raw+".jpg"
+    file_info = bot.get_file(raw)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open("photos/"+path,'wb') as new_file:
+        new_file.write(downloaded_file)
+    add(message, name_type, path)
+
+
+
+
+
+
+
+def add(message,name_type, path):
+    obg = ControllerTypeGoods(name_type)
+    obg.add_item(path)
+    bot.send_message(message.chat.id, f'Тип с названием {name_type} \n - был добавлен в базу !')
         
 
 def delete(message):
@@ -105,7 +114,7 @@ def delete(message):
 def update(message):
     name_type = message.text
     if (len(name_type)) < 1:
-        bot.send_message(message.chat.id, 'Ну напиши ты что нибудь !')
+        msg = bot.send_message(message.chat.id, 'Ну напиши ты что нибудь !')
         bot.register_next_step_handler(msg, update)
         return
     else:
@@ -141,44 +150,12 @@ def update(message):
 #      reply_markup=mk)
 #     bot.register_next_step_handler(msg, crud)
 
-# @bot.message_handler(content_types=['Добавить','Удалить', 'Изменить'])
-# def crud(message):
-#     message_user = message.text
-#     if (message_user == 'Добавить'):
-#         msg = bot.send_message(message.chat.id, 
-#         'Напишите название типа товара, который хотите добавить')
-#         bot.register_next_step_handler(msg, add)
-#     elif (message_user == 'Удалить'):
-#         msg = bot.send_message(message.chat.id, 
-#         'Напишите название типа товара, который хотите удалить')
-#         bot.register_next_step_handler(msg, delete)
-#     elif (message_user == 'Изменить'):
-#         msg = bot.send_message(message.chat.id, 
-#         'Напишите название типа товара, который хотите изменить')
-#         bot.register_next_step_handler(msg, update)
-
-
-
-
-
-
-
-
-# @bot.message_handler(commands=['show'])
-# def show(message):
-#     obj = ControllerTypeGoods()
-#     obj.show_items()
-#     bot.send_message(message.chat.id, )
-
 
 
 
 
 # bot.enable_save_next_step_handlers(delay=2)
 # bot.load_next_step_handlers()
-
-
-
 bot.polling()
 conn.close()
 
