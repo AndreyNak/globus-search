f452d94f80303570f68b022a7189572cdf34de5d
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-16 17:10:24 +0300

    some change

--- main.py
+++ main.py
@@ -4,14 +4,7 @@ from db.connect import *
 
 
 
-def update():
+   #--------------------Изменение--------------------
+# obg = ControllerTypeGoods(input('Название типа которое хочешь поменять: '))
+# obg.update_item()
     #--------------------Изменение--------------------
-    obg = ControllerTypeGoods(input('Название типа которое хочешь поменять: '))
-    obg.update_item()
-    #--------------------Изменение--------------------
-
-
-
-obj = ControllerTypeGoods()
-set = obj.show_items()
-
