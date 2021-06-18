7ea5491f99e3033a69b76489dc79a721c94bf82c
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-18 20:54:31 +0300

    main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from db.models.base_model import TypCategoryes
 from controllers.controller_type import ControllerTypeGoods
 from db.connect import *
 
@@ -8,3 +9,12 @@ from db.connect import *
 # obg = ControllerTypeGoods(input('Название типа которое хочешь поменять: '))
 # obg.update_item()
     #--------------------Изменение--------------------
+
+
+# obj = ControllerTypeGoods(input("Имя категории "))
+# cat = obj.select_category()
+
+# S = 'Восток'
+
+# print(f" Категория: {cat[0]}Отдел:{cat[1]} Крыло:{S}")
+
