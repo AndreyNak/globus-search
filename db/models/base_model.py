fad23641738d030e5b08e4cff89a3f0b44397fc6
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-18 20:57:18 +0300

    add field

--- db/models/base_model.py
+++ db/models/base_model.py
@@ -1,4 +1,5 @@
 from os import path
+from re import T
 from peewee import *
 from db.connect import *
 
@@ -12,7 +13,19 @@ class Types(BaseModel):
     id = AutoField(column_name='id')
     type = TextField(column_name='type')
     path = TextField(column_name='path')
-    
+    side = TextField(column_name='side')
 
     class Meta:
         table_name = 'Types'
+
+
+
+
+class TypCategoryes(BaseModel):
+    id = AutoField(column_name='id')
+    name = TextField(column_name='name')
+    path = TextField(column_name='path')
+    name_type = TextField(column_name='name_type', null=True)
+    
+    class Meta:
+        table_name = 'Category'
