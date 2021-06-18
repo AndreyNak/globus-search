48ea7a76f0c36ae06ec4834d40ee3d5cd69f3cba
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-18 20:53:41 +0300

    update code

--- controllers/controller_type.py
+++ controllers/controller_type.py
@@ -1,30 +1,32 @@
-from db.models.base_model import Types
+import re
+
+from telebot.types import ReplyKeyboardMarkup
+from db.models.base_model import Types,TypCategoryes
 import peewee
 import os
 
 
 class ControllerTypeGoods:
     def __init__(self, name_type=None):
-        self.name_type = name_type
+        self.name_item = name_type
     
     def select_items(self):
         cur_query = Types.select()
         return [i['type']for i in cur_query.dicts().execute()]
-        
 
-    def show_item(self):
-        item = Types.get(Types.type == self.name_type)
-        return item.path
+    def show_type(self):
+        return Types.get(Types.type == self.name_item)
+       
 
-    def add_item(self,path):
+    def add_item(self,path, side):
         path_photo = f"photos/{path}"
-        article = Types(type=self.name_type,path=path_photo)
+        article = Types(type=self.name_item,path=path_photo, side=side)
         article.save()
 
     
     def delete_item(self):
         try:
-            artist = Types.get(Types.type == self.name_type )
+            artist = Types.get(Types.type == self.name_item )
         except Exception:
             return 'Не удален, потому что не найден !'
         else:
