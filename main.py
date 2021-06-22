7652d4159b2014a7353d5588c6cea4d7534c2ae2
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-22 22:24:52 +0300

    main

--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 import telebot
 from db.models.base_model import TypCategoryes
 from controllers.controller_type import ControllerTypeGoods
+from controllers.controller_categories import ControllerCategories
 from db.connect import *
 
 
 
 
-   #--------------------Изменение--------------------
-# obg = ControllerTypeGoods(input('Название типа которое хочешь поменять: '))
-# obg.update_item()
-    #--------------------Изменение--------------------
 
-
-# obj = ControllerTypeGoods(input("Имя категории "))
-# cat = obj.select_category()
-
-# S = 'Восток'
-
-# print(f" Категория: {cat[0]}Отдел:{cat[1]} Крыло:{S}")
