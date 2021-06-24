628638852a7025d55e550689cc61a03ab32845a8
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-24 18:40:14 +0300

    main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import telebot
-from db.models.base_model import TypCategoryes
+from db.models.categoryes_model import TypCategoryes
+from db.models.types_model import Types
 from controllers.controller_type import ControllerTypeGoods
 from controllers.controller_categories import ControllerCategories
 from db.connect import *
@@ -7,4 +8,16 @@ from db.connect import *
 
 
 
+# message = input("Слово:")
 
+
+
+
+
+
+# def show_what_have(message):
+#     obg = ControllerCategories()
+#     return [i for i in obg.select_items1() if i.find(message)!= - 1]
+
+
+# print(f"Ничего не найдено, может вы имели ввиду: {show_what_have(message)}")
