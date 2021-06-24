3aab71dde05d2e4109a45014034f7f618b82d28d
canina1337 <nakaryakov.andreyka@mail.ru>
2021-06-24 18:41:54 +0300

    add type model file

--- db/models/types_model.py
+++ db/models/types_model.py
@@ -1,11 +1,13 @@
 
 from peewee import *
-from base_model import BaseModel
+from db.models.base_model import BaseModel
 
 
-# class Types(BaseModel):
-#     id = AutoField(column_name='id')
-#     type = TextField(column_name='type', null=True)
+class Types(BaseModel):
+    id = AutoField(column_name='id')
+    type = TextField(column_name='type')
+    path = TextField(column_name='path')
+    side = TextField(column_name='side')
 
-#     class Meta:
-#         table_name = 'Types'
+    class Meta:
+        table_name = 'Types'
