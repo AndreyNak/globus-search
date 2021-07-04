474691b2c59017f342eb41e24f3c12f2093549e4
canina1337 <nakaryakov.andreyka@mail.ru>
2021-07-04 13:34:02 +0300

    tests

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import path
 import requests
 import telebot
 from db.models.categoryes_model import TypCategoryes
@@ -6,9 +7,22 @@ from controllers.controller_type import ControllerTypeGoods
 from controllers.controller_categories import ControllerCategories
 from db.connect import *
 import time
+from pyaspeller import YandexSpeller
 
 
 
+#my_request = input(":")
 
 
+
+def speller_filter(message):
+    speller = YandexSpeller()
+    fixed = speller.spelled(message)
+    if (message != fixed):
+        return fixed
+    return message
+
+
+# str = speller_filter(my_request)
+# print(str)
 
